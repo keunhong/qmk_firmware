72943f33d3dee90be5de5043a129006ac911d104
Keunhong Park <kpar@cs.washington.edu>
2019-05-25 17:55:45 -0700

    Fix modtap

--- keyboards/keebio/iris/keymaps/kpar/config.h
+++ keyboards/keebio/iris/keymaps/kpar/config.h
@@ -34,3 +34,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGBLIGHT_HUE_STEP 8
 #define RGBLIGHT_SAT_STEP 8
 #define RGBLIGHT_VAL_STEP 8
+
+#ifdef TAPPING_TERM
+#undef TAPPING_TERM
+#endif // TAPPING_TERM
+#define TAPPING_TERM 175
+#define PERMISSIVE_HOLD
+#define IGNORE_MOD_TAP_INTERRUPT
+
